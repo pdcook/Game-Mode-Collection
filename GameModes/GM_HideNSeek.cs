d42367449907e1c1807a1c7cc79c2f6c8fa323e2
tddebart <tddebart@gmail.com>
2022-02-06 19:57:07 +0100

    finally fixed hide&seek

--- GameModes/GM_HideNSeek.cs
+++ GameModes/GM_HideNSeek.cs
@@ -27,7 +27,7 @@ namespace GameModeCollection.GameModes
     /// The hiders get 1 point for every seeker they kill if the hiders win the round.
     /// The seekers get (amount of seekers) points * (percentage of hiders killed) if the seekers win the round.
     ///
-    /// NOTE: If this gamemode is played with cards that don't remove themself properly some thing will go wrong.
+    /// NOTE: If this gamemode is played with cards that don't remove themself properly some thing WILL  go wrong.
     /// </summary>
     public class GM_HideNSeek : RWFGameMode
     {
@@ -36,9 +36,6 @@ namespace GameModeCollection.GameModes
         public List<int> hiderIDs = new List<int>();
         public Dictionary<int, Player> players = new Dictionary<int, Player>();
         public Dictionary<int, int> otherTeamKills = new Dictionary<int, int>();
-        public Dictionary<int, int> pointsToGive = new Dictionary<int, int>();
-        public List<int> deathPlayers = new List<int>();
-        public int[] lastPointWinningTeamIDs;
         public float timeLimit => this.hiderIDs.Count*15f;
         public float timeLeft;
         public bool startTimer = false;
@@ -50,19 +47,6 @@ namespace GameModeCollection.GameModes
         {
             base.Awake();
             instance = this;
-        }
-
-        private void OnEnable()
-        {
-            base.OnDisable();
-            GameModeManager.AddHook(GameModeHooks.HookBattleStart, this.OnBattleStart);
-        }
-
-
-        public override void OnDisable()
-        {
-            base.OnDisable();
-            GameModeManager.RemoveHook(GameModeHooks.HookBattleStart, this.OnBattleStart);
         }
 
         public override void StartGame()
@@ -81,7 +65,6 @@ namespace GameModeCollection.GameModes
             // Reset the game mode
             this.seekerIDs.Clear();
             this.hiderIDs.Clear();
-            this.deathPlayers.Clear();
 
 
             // Reset things
@@ -97,44 +80,55 @@ namespace GameModeCollection.GameModes
 
             if (PhotonNetwork.IsMasterClient)
             {
-                this.ExecuteAfterFrames(1, () =>
+                // Get seekerIDs
+                var amountOfSeekers = Mathf.Floor(instance.players.Count / 3);
+                if(amountOfSeekers == 0) { amountOfSeekers = 1; }
+                var localSeekerIDs = new List<int>();
+                // Set the seeker IDs
+                for (int i = 0; i < amountOfSeekers; i++)
                 {
-                    var seed = Random.Range(0, int.MaxValue);
+                    var randomIndex = Random.Range(0, instance.players.Count);
+                    // If the ID is already in the list, try again
+                    while (instance.seekerIDs.Contains(instance.players[randomIndex].playerID))
+                    {
+                        randomIndex = Random.Range(0, instance.players.Count);
+                    }
+                    localSeekerIDs.Add(randomIndex);
+                }
+
+                // this.ExecuteAfterFrames(1, () =>
+                // {
                     NetworkingManager.RPC(
                         typeof(GM_HideNSeek),
                         nameof(GM_HideNSeek.RPCA_GenerateTeams),
-                        seed
+                        localSeekerIDs.ToArray()
                     );
-                });
+                // });
             }
         }
 
         [UnboundRPC]
-        public static void RPCA_GenerateTeams(int seed)
+        public static void RPCA_GenerateTeams(int[] localSeekerIDs)
         {
+            
             var instance = GM_HideNSeek.instance;
             
-            var rnd = new System.Random(seed);
-
-            var amountOfSeekers = Mathf.Floor(instance.players.Count / 3);
-            if(amountOfSeekers == 0) { amountOfSeekers = 1; }
-            // Set the seeker IDs
-            for (int i = 0; i < amountOfSeekers; i++)
+            if (!PhotonNetwork.IsMasterClient)
             {
-                var randomIndex = rnd.Next(0, instance.players.Count);
-                // If the ID is already in the list, try again
-                while (instance.seekerIDs.Contains(instance.players[randomIndex].playerID))
-                {
-                    randomIndex = rnd.Next(0, instance.players.Count);
-                }
-                instance.seekerIDs.Add(instance.players[randomIndex].playerID);
+                instance.ResetGame();
+            }
+            // Set the seeker IDs
+            foreach (var index in localSeekerIDs)
+            {
+                instance.seekerIDs.Add(instance.players[index].playerID);
                 GameModeCollection.instance.ExecuteAfterFrames(2, () =>
                 {
-                    var colorEffect = instance.players[randomIndex].gameObject.GetOrAddComponent<ReversibleColorEffect>();
+                    var colorEffect = instance.players[index].gameObject.GetOrAddComponent<ReversibleColorEffect>();
+                    colorEffect.SetLivesToEffect(100);
                     colorEffect.SetColor(Color.red);
                     colorEffect.ApplyColor();
                 });
-                GameModeCollection.Log("[Hide&Seek] Seeker ID: " + instance.players[randomIndex].playerID);
+                GameModeCollection.Log("[Hide&Seek] Seeker ID: " + instance.players[index].playerID);
             }
 
             // Set the hider IDs
@@ -143,23 +137,10 @@ namespace GameModeCollection.GameModes
                 instance.hiderIDs.Add(player.playerID);
                 Cards.instance.AddCardToPlayer(player, HiderCard.instance, addToCardBar: false);
 
-                // GameModeCollection.Log("Hider ID: " + player.playerID);
+                // GameModeCollection.Log("[Hide&Seek] Hider ID: " + player.playerID);
             }
             
             instance.timeLeft = instance.timeLimit;
-        }
-
-        public IEnumerator OnBattleStart(IGameModeHandler handler)
-        {
-            foreach (var player in this.players.Values)
-            {
-                if (player.GetComponent<ReversibleColorEffect>())
-                {
-                    player.GetComponent<ReversibleColorEffect>().ApplyColor();
-                }
-            }
-
-            yield return null;
         }
 
         public override void PlayerJoined(Player player)
@@ -223,11 +204,6 @@ namespace GameModeCollection.GameModes
 
         public override void PlayerDied(Player killedPlayer, int teamsAlive)
         {
-            if (this.deathPlayers.Contains(killedPlayer.playerID))
-            {
-                return;
-            }
-
             var playerKiller = PlayerManager.instance.players.FirstOrDefault(p => p.data.lastDamagedPlayer == killedPlayer &&
                                                                          (float)p.data.stats.GetFieldValue("sinceDealtDamage") <= 1.5f && !p.data.dead);
             if (playerKiller != null && playerKiller != killedPlayer)
@@ -248,8 +224,6 @@ namespace GameModeCollection.GameModes
                 //     this.otherTeamKills[playerKiller.playerID]--;
                 // }
             }
-            
-            this.deathPlayers.Add(killedPlayer.playerID);
 
             // Check if all hiders are dead
             if(this.hiderIDs.Select(id => this.players[id]).All(p => p.data.dead))
@@ -390,7 +364,10 @@ namespace GameModeCollection.GameModes
 
         public override IEnumerator DoRoundStart()
         {
-            this.ResetGame();
+            if (PhotonNetwork.IsMasterClient)
+            {
+                this.ResetGame();
+            }
             yield return base.DoRoundStart();
             this.HideNSeekCO = this.StartCoroutine(this.DoHideNSeek());
             
@@ -404,7 +381,10 @@ namespace GameModeCollection.GameModes
         }
         public override IEnumerator DoPointStart()
         {
-            this.ResetGame();
+            if (PhotonNetwork.IsMasterClient)
+            {
+                this.ResetGame();
+            }
             yield return base.DoPointStart();
             this.HideNSeekCO = this.StartCoroutine(this.DoHideNSeek());
             
